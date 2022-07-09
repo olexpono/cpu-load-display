740ffe70d5bda169a397d4b3ed75995f24d11f23
Olex Ponomarenko <tholex@gmail.com>
2022-07-08 23:57:05 -0400

    Save after-dinner work

--- app.vue
+++ app.vue
@@ -1,19 +1,27 @@
 <template>
-  <h1>CPU Load Tracker</h1>
-  <pre>Infatuation take-home, 2022-07-08</pre>
-  <button @click="() => refresh()">Fetch CPU</button>
-  <pre v-if="error">Error loading CPU Data</pre>
-  <pre>{{ JSON.stringify(latestData, null, 2) }}</pre>
-
-  <button @click="toggleMonitoring">{{ isMonitoring ? "Turn off monitoring" : "Turn on monitoring"}}</button>
-  <div id="cpuChart"></div>
+  <main class="main">
+    <pre>Infatuation take-home, 2022-07-08</pre>
+    <!--<button @click="() => refresh()">Fetch CPU</button>
+    <pre v-if="error">Error loading CPU Data</pre>
+    <pre>{{ JSON.stringify(latestData, null, 2) }}</pre>
+    -->
+    <button @click="toggleMonitoring">{{ isMonitoring ? "Turn off monitoring" : "Turn on monitoring"}}</button>
+    <button @click="toggleRenderAnimation()">{{ isRenderingFast ? "Render slower" : "Render continuously" }}</button>
+    <h3>CPU Load (1 min avg)</h3>
+    <div id="cpuChart"></div>
+  </main>
 </template>
 
 <script setup lang="ts">
 import { Timeline, Graph2d, moment, Graph2dOptions } from "vis-timeline/standalone";
 
-const REFRESH_RATE = 1000; // 10X before submitting XD ms
+const REFRESH_RATE = 10000;
+const START_CONTINUOUS_RENDER = true;
+
 const graph2d = ref<null | Graph2d>(null);
+
+const isRenderingFast = ref(false);
+const toggleRenderAnimation = () => isRenderingFast.value = !isRenderingFast.value;
 
 const {
   dataset,
@@ -22,15 +30,33 @@ const {
   refresh,
   isMonitoring,
   toggleMonitoring
-} = await useCpuMonitor();
+} = await useCpuMonitor(REFRESH_RATE);
+
+const snapWindowToDefaultRange = () => {
+  if (graph2d.value == null) { return; }
+  graph2d.value.setWindow(
+    moment().subtract(4, 'minutes').toDate(),
+    moment().add(25, 'seconds').toDate(),
+    { animation: false }
+  );
+}
 
 watch(latestData, () => {
-  if (graph2d.value !== null) {
-    graph2d.value.setWindow(
-      moment().subtract(4, 'minutes').toDate(),
-      moment().add(25, 'seconds').toDate(),
-      { animation: false }
-    );
+  if(!isRenderingFast.value) {
+    snapWindowToDefaultRange();
+  }
+})
+
+const renderContinuously = () => {
+  snapWindowToDefaultRange();
+  if (isRenderingFast.value) {
+    requestAnimationFrame(renderContinuously)
+  }
+}
+
+watch(isRenderingFast, () => {
+  if (isRenderingFast.value) {
+    renderContinuously();
   }
 })
 
@@ -42,8 +68,8 @@ onMounted(() => {
     dataAxis: {
       left: {
         range: {
-          min: -0.1,
-          max: 1.1,
+          min: -0.08,
+          max: 1.08,
         },
       },
     },
@@ -54,17 +80,54 @@ onMounted(() => {
   };
 
   graph2d.value = new Graph2d(container, dataset, options);
+  refresh();
+  if (START_CONTINUOUS_RENDER) {
+    setTimeout(() => {
+      isRenderingFast.value = true;
+      renderContinuously();
+    }, 12500);
+  }
 })
 </script>
 
 <style>
 html, body { margin: 0; }
+* {
+  box-sizing: inherit;
+}
+:root {
+  --white: #fafafa;
+  --fg: #dadada;
+  --darkgray: #5a5a5a;
+}
+
 body {
   box-sizing: border-box;
   font-family: -apple-system, BlinkMacSystemFont, 'Inter', sans-serif;
   padding: 0 30px;
+  color: var(--fg);
+  background-color: #353535;
 }
-* {
-  box-sizing: inherit;
+
+.main {
+  max-width: 920px;
+  margin: 0 auto;
+}
+
+
+/* Visjs edits */
+.vis-timeline {
+  border-color: var(--darkgray);
+}
+.vis-data-axis .vis-y-axis.vis-major {
+  color: var(--white);
+}
+
+.vis-time-axis .vis-text {
+  color: var(--fg);
+}
+.vis-panel.vis-background.vis-horizontal .vis-grid.vis-minor,
+.vis-panel.vis-background.vis-vertical .vis-grid.vis-minor {
+  border-color: var(--darkgray);
 }
 </style>
--- composables/useCpuMonitor.ts
+++ composables/useCpuMonitor.ts
@@ -1,11 +1,61 @@
-import { DataSet } from "vis-timeline/standalone";
+import { DataSet, moment } from "vis-timeline/standalone";
 
-const dataset = new DataSet();
-const isMonitoring = ref(true);
+type LoadStatus = "ok" | "overloaded";
+type StatusPeriod = {
+  status: LoadStatus,
+  startTime: number,
+  endInclusive: number | null
+}
+type AlertsData = {
+  statusHistory: StatusPeriod[],
+  triggeredStatus: null | LoadStatus,
+}
 
-const toggleMonitoring = () => isMonitoring.value = !isMonitoring.value;
+const LOAD_THRESHOLD = 1;
+
+const computeAlertsData = (dataset, threshold): AlertsData => {
+  const dataPoints = dataset.get() as {x: number, y: number}[];
+  const statusHistory: StatusPeriod[] = [];
+  let currentStatus = null;
+  let lastTimestamp = null;
+  let triggeredStatus: null | LoadStatus = null;
+
+  dataPoints.forEach((point) => {
+    const status = point > threshold ? "overloaded" : "ok";
+    triggeredStatus = null;
+
+    if (status !== currentStatus) {
+      if (statusHistory.length > 0 && lastTimestamp) {
+        statusHistory.at(-1).endInclusive = lastTimestamp;
+      }
+
+      statusHistory.push({
+        status,
+        startTime: point.x,
+        endInclusive: null,
+      })
+      triggeredStatus = status;
+    }
+
+    lastTimestamp = point.x;
+  })
+
+  return {
+    statusHistory: [],
+    triggeredStatus,
+  }
+}
 
 export const useCpuMonitor = async (interval = 10000) => {
+  const dataset = new DataSet();
+  const isMonitoring = ref(true);
+  const alertsData = ref<AlertsData>({
+    statusHistory: [] as StatusPeriod[],
+    triggeredStatus: null
+  })
+
+  const toggleMonitoring = () => isMonitoring.value = !isMonitoring.value;
+
   const { data: cpuData, refresh, error } = await useFetch('/api/cpu-load')
 
   setInterval(() => {
@@ -13,11 +63,20 @@ export const useCpuMonitor = async (interval = 10000) => {
     refresh();
   }, interval)
 
+  // Runs whenever a new data point arrives
   watch(cpuData, (newData) => {
     dataset.add({
       x: newData.timestamp,
-      y: newData.load
+      y: newData.load,
+      group: 1
     } as any)
+
+    const idsToRemove = dataset.getIds({
+      filter: (item: any) => moment().subtract(10, 'minutes').isBefore(item.x)
+    });
+    dataset.remove(idsToRemove);
+
+    alertsData.value = computeAlertsData(dataset, LOAD_THRESHOLD);
   })
 
   return {
